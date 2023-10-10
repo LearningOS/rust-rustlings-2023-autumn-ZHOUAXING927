ed32e8799805739a8a700fa2e1bcb2cf80e1fb08
zhouaxing <274396252@qq.com>
2023-10-10 17:50:09 +0800

    update

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -3,14 +3,14 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn main() {
-    my_macro!();
-}
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
 }
+//宏 比 main 加载的快
+fn main() {
+    my_macro!();
+}
+
+
--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,8 +5,10 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+#[macro_use]
+//#[macro_use] 是 Rust 编程语言中的一个属性，用于宏（macro）的使用者表明他们希望在代码中使用这个宏。
+//当编译器遇到 #[macro_use] 时，它会检查宏定义中的 include 属性，
+//并确保在使用宏的地方包含了正确的头文件。这有助于避免因缺少头文件而导致的编译错误。
 mod macros {
     macro_rules! my_macro {
         () => {
