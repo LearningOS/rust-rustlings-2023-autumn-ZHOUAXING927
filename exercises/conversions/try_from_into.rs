96de54ff4c6218f6d33753f2362484017dda45ce
zhouaxing <274396252@qq.com>
2023-10-11 11:57:47 +0800

    update

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -10,6 +10,7 @@
 // a hint.
 
 use std::convert::{TryFrom, TryInto};
+use std::error;
 
 #[derive(Debug, PartialEq)]
 struct Color {
@@ -27,7 +28,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
@@ -41,6 +41,19 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        //let (red, green, blue) = tuple;
+        let color = Color {
+            red: u8::try_from(tuple.0).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+            green: u8::try_from(tuple.1).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+            blue: u8::try_from(tuple.2).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+        };
+        Ok(color)
     }
 }
 
@@ -48,6 +61,18 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let color = Color {
+            red: u8::try_from(arr[0]).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+            green: u8::try_from(arr[1]).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+            blue: u8::try_from(arr[2]).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+        };
+        Ok(color)
     }
 }
 
@@ -55,6 +80,21 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let color = Color {
+            red: u8::try_from(slice[0]).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+            green: u8::try_from(slice[1]).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+            blue: u8::try_from(slice[2]).map_err(|_| {
+                IntoColorError::IntConversion
+            })?,
+        };
+        Ok(color)
     }
 }
 
@@ -87,6 +127,7 @@ mod tests {
             Err(IntoColorError::IntConversion)
         );
     }
+
     #[test]
     fn test_tuple_out_of_range_negative() {
         assert_eq!(
@@ -94,6 +135,7 @@ mod tests {
             Err(IntoColorError::IntConversion)
         );
     }
+
     #[test]
     fn test_tuple_sum() {
         assert_eq!(
@@ -101,6 +143,7 @@ mod tests {
             Err(IntoColorError::IntConversion)
         );
     }
+
     #[test]
     fn test_tuple_correct() {
         let c: Result<Color, _> = (183, 65, 14).try_into();
@@ -110,25 +153,29 @@ mod tests {
             Color {
                 red: 183,
                 green: 65,
-                blue: 14
+                blue: 14,
             }
         );
     }
+
     #[test]
     fn test_array_out_of_range_positive() {
         let c: Result<Color, _> = [1000, 10000, 256].try_into();
         assert_eq!(c, Err(IntoColorError::IntConversion));
     }
+
     #[test]
     fn test_array_out_of_range_negative() {
         let c: Result<Color, _> = [-10, -256, -1].try_into();
         assert_eq!(c, Err(IntoColorError::IntConversion));
     }
+
     #[test]
     fn test_array_sum() {
         let c: Result<Color, _> = [-1, 255, 255].try_into();
         assert_eq!(c, Err(IntoColorError::IntConversion));
     }
+
     #[test]
     fn test_array_correct() {
         let c: Result<Color, _> = [183, 65, 14].try_into();
@@ -138,10 +185,11 @@ mod tests {
             Color {
                 red: 183,
                 green: 65,
-                blue: 14
+                blue: 14,
             }
         );
     }
+
     #[test]
     fn test_slice_out_of_range_positive() {
         let arr = [10000, 256, 1000];
@@ -150,6 +198,7 @@ mod tests {
             Err(IntoColorError::IntConversion)
         );
     }
+
     #[test]
     fn test_slice_out_of_range_negative() {
         let arr = [-256, -1, -10];
@@ -158,6 +207,7 @@ mod tests {
             Err(IntoColorError::IntConversion)
         );
     }
+
     #[test]
     fn test_slice_sum() {
         let arr = [-1, 255, 255];
@@ -166,6 +216,7 @@ mod tests {
             Err(IntoColorError::IntConversion)
         );
     }
+
     #[test]
     fn test_slice_correct() {
         let v = vec![183, 65, 14];
@@ -176,15 +227,17 @@ mod tests {
             Color {
                 red: 183,
                 green: 65,
-                blue: 14
+                blue: 14,
             }
         );
     }
+
     #[test]
     fn test_slice_excess_length() {
         let v = vec![0, 0, 0, 0];
         assert_eq!(Color::try_from(&v[..]), Err(IntoColorError::BadLen));
     }
+
     #[test]
     fn test_slice_insufficient_length() {
         let v = vec![0, 0];
--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -1,40 +1,45 @@
 // tests9.rs
 //
-// Rust is highly capable of sharing FFI interfaces with C/C++ and other statically compiled
-// languages, and it can even link within the code itself! It makes it through the extern
+// rust is highly capable of sharing ffi interfaces with c/c++ and other statically compiled
+// languages, and it can even link within the code itself! it makes it through the extern
 // block, just like the code below.
 //
-// The short string after the `extern` keyword indicates which ABI the externally imported
-// function would follow. In this exercise, "Rust" is used, while other variants exists like
-// "C" for standard C ABI, "stdcall" for the Windows ABI.
+// the short string after the `extern` keyword indicates which abi the externally imported
+// function would follow. in this exercise, "rust" is used, while other variants exists like
+// "c" for standard c abi, "stdcall" for the windows abi.
 //
-// The externally imported functions are declared in the extern blocks, with a semicolon to
-// mark the end of signature instead of curly braces. Some attributes can be applied to those
+// the externally imported functions are declared in the extern blocks, with a semicolon to
+// mark the end of signature instead of curly braces. some attributes can be applied to those
 // function declarations to modify the linking behavior, such as #[link_name = ".."] to
 // modify the actual symbol names.
 //
-// If you want to export your symbol to the linking environment, the `extern` keyword can
-// also be marked before a function definition with the same ABI string note. The default ABI
-// for Rust functions is literally "Rust", so if you want to link against pure Rust functions,
+// if you want to export your symbol to the linking environment, the `extern` keyword can
+// also be marked before a function definition with the same abi string note. the default abi
+// for rust functions is literally "rust", so if you want to link against pure rust functions,
 // the whole extern term can be omitted.
 //
-// Rust mangles symbols by default, just like C++ does. To suppress this behavior and make
+// rust mangles symbols by default, just like c++ does. to suppress this behavior and make
 // those functions addressable by name, the attribute #[no_mangle] can be applied.
 //
-// In this exercise, your task is to make the testcase able to call the `my_demo_function` in
-// module Foo. the `my_demo_function_alias` is an alias for `my_demo_function`, so the two
+// in this exercise, your task is to make the testcase able to call the `my_demo_function` in
+// module foo. the `my_demo_function_alias` is an alias for `my_demo_function`, so the two
 // line of code in the testcase should call the same function.
 //
-// You should NOT modify any existing code except for adding two lines of attributes.
+// you should not modify any existing code except for adding two lines of attributes.
 
 // I AM NOT DONE
 
 extern "Rust" {
+    //#[link_name]是一个属性元标量（attribute macro），用于在编译时设置符号链接名称
+    #[link_name = "my_demo_function"]
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
+    //这个属性指示Rust编译器在编译时不要改变这个函数的名字。这意味着这个函数在链接时将保留其原始名字和签名。
+    #[no_mangle]
     // No `extern` equals `extern "Rust"`.
     fn my_demo_function(a: u32) -> u32 {
         a
